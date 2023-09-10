604942e1300227fc7340a1e5c370ee279299b3da
Roman Maliutin <maliutinroman@gmail.com>
2023-09-10 22:24:51 +0300

    Add GelfTarget for Graylog

--- CatalogWindow/CatalogWindow/Parts.xaml.cs
+++ CatalogWindow/CatalogWindow/Parts.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using NLog;
+using System.Diagnostics;
+using System.Windows;
 
 namespace CatalogWindow
 {
@@ -8,22 +10,35 @@ namespace CatalogWindow
     public partial class Parts : Window
     {
         private static readonly NLog.Logger logger = Logger.GetLogger();
+        private readonly Stopwatch stopwatch = new Stopwatch();
 
         public Parts()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            stopwatch.Start();            
         }
 
         private void OnButtonClickRecommend(object sender, RoutedEventArgs e)
         {
-            logger.Info("Button_Recommend clicked");
+            stopwatch.Stop();
+            logger.Info(GetLogEventInfo("CA 2000", true, stopwatch.Elapsed.TotalSeconds));
             Close();
         }
 
         private void OnButtonClickSearch(object sender, RoutedEventArgs e)
         {
-            logger.Warn("Button_Search clicked");
+            stopwatch.Stop();
+            logger.Info(GetLogEventInfo("SLIP22.1", false, stopwatch.Elapsed.TotalSeconds));
             Close();
+        }
+
+        private LogEventInfo GetLogEventInfo(string partId, bool recommended, double elapsedSeconds)
+        {
+            LogEventInfo eventInfo = new LogEventInfo { Message = "Part selected" };
+            eventInfo.Properties.Add("part_id", partId);
+            eventInfo.Properties.Add("recommended", recommended.ToString());
+            eventInfo.Properties.Add("elapsed_seconds", elapsedSeconds);
+            return eventInfo;
         }
     }
 }
