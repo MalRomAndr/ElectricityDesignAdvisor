ae416b7e0360d7ecbb0ec0c328871b38d313a53d
Roman Maliutin <maliutinroman@gmail.com>
2023-09-13 20:16:18 +0300

    Add more data to logging

--- CatalogWindow/CatalogWindow/Parts.xaml.cs
+++ CatalogWindow/CatalogWindow/Parts.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 
@@ -12,19 +13,30 @@ namespace CatalogWindow
         private static readonly NLog.Logger logger = Logger.GetLogger();
         private readonly Stopwatch stopwatch = new Stopwatch();
 
+        private readonly List<string> parts = new List<string>()
+        {
+            "SO250.01R",
+            "СВ95-3",
+            "COT36R",
+            "COT37R",
+            "SOT29.10R",
+        };
+
         public Parts()
         {
-            InitializeComponent();            
-            stopwatch.Start();            
+            InitializeComponent();
+            stopwatch.Start();
         }
 
         private void OnButtonClickRecommend(object sender, RoutedEventArgs e)
         {
             stopwatch.Stop();
+            LogEventInfo eventInfo = MakeLogEventInfo(
+                "CA 2000", true, stopwatch.Elapsed.TotalSeconds, parts, "А10-2", "support", "1.0.0.0");
 #if DEBUG
-            logger.Debug(GetLogEventInfo("CA 2000", true, stopwatch.Elapsed.TotalSeconds));
+            logger.Debug(eventInfo);
 #else
-            logger.Info(GetLogEventInfo("CA 2000", true, stopwatch.Elapsed.TotalSeconds));
+            logger.Info(eventInfo);
 #endif
             Close();
         }
@@ -32,20 +44,33 @@ namespace CatalogWindow
         private void OnButtonClickSearch(object sender, RoutedEventArgs e)
         {
             stopwatch.Stop();
+            LogEventInfo eventInfo = MakeLogEventInfo(
+                "CA 2000", false, stopwatch.Elapsed.TotalSeconds, parts, "А10-2", "support", "1.0.0.0");
 #if DEBUG
-            logger.Debug(GetLogEventInfo("CA 2000", false, stopwatch.Elapsed.TotalSeconds));
+            logger.Debug(eventInfo);
 #else
-            logger.Info(GetLogEventInfo("CA 2000", false, stopwatch.Elapsed.TotalSeconds));
+            logger.Info(eventInfo);
 #endif
             Close();
         }
 
-        private LogEventInfo GetLogEventInfo(string partId, bool recommended, double elapsedSeconds)
+        private LogEventInfo MakeLogEventInfo(
+            string partId,
+            bool recommended,
+            double elapsedSeconds,
+            List<string> parts,
+            string structureName,
+            string structureType,
+            string appVersion)
         {
             LogEventInfo eventInfo = new LogEventInfo { Message = "Part selected" };
             eventInfo.Properties.Add("part_id", partId);
             eventInfo.Properties.Add("recommended", recommended.ToString());
             eventInfo.Properties.Add("elapsed_seconds", elapsedSeconds);
+            eventInfo.Properties.Add("parts", parts);
+            eventInfo.Properties.Add("structure_name", structureName);
+            eventInfo.Properties.Add("structure_type", structureType);
+            eventInfo.Properties.Add("app_verison", appVersion);
             return eventInfo;
         }
     }
