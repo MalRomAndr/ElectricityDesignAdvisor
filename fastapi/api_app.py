60fc97399a3462a4aa2c272d8c5c0b6540d68eed
Roman Maliutin <maliutinroman@gmail.com>
2023-11-04 00:01:07 +0300

    WIP Fix fastapi file paths

--- fastapi/api_app.py
+++ fastapi/api_app.py
@@ -1,17 +1,17 @@
 import joblib
 import sys
-import os
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
-model_path = os.path.dirname(os.path.abspath('../model/fpgrowth.py'))
-sys.path.append(model_path)
+from pydantic import BaseModel
 
+# Добавляем в системные пути папку с классом модели (fpgrowth.py),
+# чтобы потом она успешно загрузилась:
+sys.path.append("../shared/model")
 
-model = joblib.load('../model/data/model')
+# Грузим модель:
+model = joblib.load('../shared/model/model')
 
 app = FastAPI()
-
 
 class Item(BaseModel):
     structure_type: str
