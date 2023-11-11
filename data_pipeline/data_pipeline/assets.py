bb2aaf8e4f88a8ed4fc372d7ed8dcb08d1b0195b
Roman Maliutin <maliutinroman@gmail.com>
2023-11-12 01:32:15 +0300

    WIP Minor refactoring

--- data_pipeline/data_pipeline/assets.py
+++ data_pipeline/data_pipeline/assets.py
@@ -1,9 +1,8 @@
-import sys
 import os
 from dagster import asset
 from data_repo import DataRepo
 
-worker = DataRepo()
+repo = DataRepo()
 
 db_path = os.path.abspath("../shared/data/database.db")
 url = "http://dev.lep10.ru:9000/api/search/messages"
@@ -11,38 +10,38 @@ url = "http://dev.lep10.ru:9000/api/search/messages"
 
 @asset(io_manager_key="data")
 def df():
-    return worker.create_df(db_path)
+    return repo.create_df(db_path)
 
 
 @asset(io_manager_key="data_temp", deps=[df])
 def new_typical_transactions():
-    return worker.get_dataset(by='db', path=db_path)
+    return repo.get_dataset(by='db', path=db_path)
 
 
 @asset(io_manager_key="data", deps=[new_typical_transactions])
 def typical_transactions():
-    return worker.concat_dataset(df1_path="../shared/data/typical_transactions",
+    return repo.concat_dataset(df1_path="../shared/data/typical_transactions",
                                  df2_path="data_temp/new_typical_transactions")
 
 
 @asset(io_manager_key="data", deps=[typical_transactions])
 def all_transactions():
-    return worker.concat_dataset(df1_path="../shared/data/all_transactions",
+    return repo.concat_dataset(df1_path="../shared/data/all_transactions",
                                  df2_path="data_temp/new_typical_transactions")
 
 
 @asset(io_manager_key="data_temp")
 def new_users_transactions():
-    return worker.get_dataset(by='api', path=url)
+    return repo.get_dataset(by='api', path=url)
 
 
 @asset(io_manager_key="data_temp", deps=[new_users_transactions])
 def users_transactions_by_api():
-    return worker.concat_dataset(df1_path="../shared/data/users_transactions",
+    return repo.concat_dataset(df1_path="../shared/data/users_transactions",
                                  df2_path="data_temp/new_users_transactions")
 
 
 @asset(io_manager_key="data_temp", deps=[users_transactions_by_api])
 def all_transactions_by_api():
-    return worker.concat_dataset(df1_path="../shared/data/all_transactions",
+    return repo.concat_dataset(df1_path="../shared/data/all_transactions",
                                  df2_path="data_temp/new_users_transactions")
