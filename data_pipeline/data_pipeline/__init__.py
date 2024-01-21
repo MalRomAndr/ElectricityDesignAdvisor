385c435baddbc8e74ea8ea17c4f2841b75bb6dde
Roman Maliutin <maliutinroman@gmail.com>
2024-01-21 17:53:33 +0300

    Delete shedules

--- data_pipeline/data_pipeline/__init__.py
+++ data_pipeline/data_pipeline/__init__.py
@@ -1,9 +1,11 @@
+"""
+Инициализация дагстера
+"""
 from dagster import Definitions, load_assets_from_modules, FilesystemIOManager
 from dotenv import load_dotenv
 from . import assets
 from . import jobs
 from . import sensors
-from . import shedules
 
 load_dotenv()   # Загрузить переменные окружения из файла .env
 all_assets = load_assets_from_modules([assets])
@@ -12,7 +14,6 @@ defs = Definitions(
     assets=all_assets,
     jobs=[jobs.overlap_checking_job],
     sensors=[sensors.check_db_changes],
-    schedules=[shedules.update_dataset_by_api, shedules.overlap_checking],
     resources={
         "data": FilesystemIOManager(base_dir="../shared/data"),
         "data_temp": FilesystemIOManager(base_dir="data_temp/")
